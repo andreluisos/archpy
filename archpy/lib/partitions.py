5a2059185c31bb7783e3367c33ea221faf7d12ab
André Luís <andreluisos@msn.com>
2021-11-24 17:40:58 -0300

    Version 1.0.3 - Added support for BIOS systems with GRUB.

--- archpy/lib/partitions.py
+++ archpy/lib/partitions.py
@@ -65,6 +65,7 @@ class Partition:
             system_partitions.append(f'/dev/disk/by-partlabel/system{index}')
 
         print(system_partitions)
+        exit()
         # if self.config['raid'] and filesystem == 'BTRFS':
         #     Cmd(f'mkfs.btrfs -L {self.config["hostname"]} -d {self.config["raid"]} -m {self.config["raid"]} -f '
         #         f'{" ".join(system_partitions)}',
